a2a2a6dc116486a096c12069a8abcd1803de03b8
Xdebx <86518562+Xdebx@users.noreply.github.com>
2022-03-30 19:27:19 +0800

    Konting edit

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,17 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-class Cart 
+use Session;
+class Cart
 {
-    use HasFactory;
-
-    // public $primaryKey = 'item_id';
-    // public $table = 'item';
-    // public $timestamps = false;
-
-    // protected $fillable = ['description','sell_price','cost_price','img_path'
-    // ];
+        public $items = null;
+        public $totalQty = 0;
+    	public $totalPrice = 0;
 
     public function __construct($oldCart) {
         if($oldCart) {
@@ -23,16 +18,19 @@ class Cart
             $this->totalQty = $oldCart->totalQty;
             $this->totalPrice = $oldCart->totalPrice;
         }
-   public function add($item, $id){
+    }
+    public function add($item, $id){
         //dd($this->items);
         $storedItem = ['qty'=>0, 'price'=>$item->sell_price, 'item'=> $item];
-        if ($this->items){
-            if (array_key_exists($id, $this->items)){
+        if ($this->items)
+        {
+            if (array_key_exists($id, $this->items))
+            {
                 $storedItem = $this->items[$id];
             }
         }
        //$storedItem['qty'] += $item->qty;
-       $storedItem['qty']++;
+        $storedItem['qty']++;
         $storedItem['price'] = $item->sell_price * $storedItem['qty'];
         $this->items[$id] = $storedItem;
         $this->totalQty++;
@@ -47,11 +45,12 @@ class Cart
         if ($this->items[$id]['qty'] <= 0) {
             unset($this->items[$id]);
         }
+    }
         public function removeItem($id){
         $this->totalQty -= $this->items[$id]['qty'];
         $this->totalPrice -= $this->items[$id]['price'];
         unset($this->items[$id]);
     }
-
 }
+
 
